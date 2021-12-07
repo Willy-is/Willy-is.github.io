a0185023053d17f73879df619880218f04785b9b
Willy0910 <57608628+Willy0910@users.noreply.github.com>
2021-12-07 16:03:32 +1100

    JS: project filtering

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
-})
+});
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -21,20 +21,20 @@ navbarMenu.addEventListener('click', (event) => {
     }
 
     scrollIntoView(link);
-})
+});
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
-})
+});
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
-})
+});
 
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
@@ -44,12 +44,37 @@ document.addEventListener('scroll', () => {
     } else {
         arrowUp.classList.remove('visible');
     }
-})
+});
 
 // Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
-})
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
+
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
