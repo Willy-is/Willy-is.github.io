6212bb3b03ddb25c5db954d81d200fb46cc63ef1
William Kim <57608628+Willy-is@users.noreply.github.com>
2022-01-11 22:49:46 +1100

    HTML & CSS: update home section and colour themes

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ homeContactBtn.addEventListener('click', () => {
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('.home__container');
+const home = document.querySelector('#home');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
